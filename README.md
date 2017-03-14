116a6f489b27748d6d4422877c6c6f70b82b25a5
Alexandre Ferreira <alexandresgf@gmail.com>
2017-03-14 15:51:07 -0300

    Added recursive code
    The function works recursive now. Still need to be reviewed.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Pre-employment test for GeoRanker
 
 This test will be made using these technologies below:
-- PHP (sitemap.xml extraction logic)
+- PHP (webscrapper logic)
 - jQuery (Ajax calls)
--- page_counter.php
+++ page_counter.php
@@ -1,44 +1,66 @@
 <?php
 // disable php error report
-error_reporting(0);
+// error_reporting(0);
+set_time_limit(0);
 
 if (!empty($_GET)) {
-    $url = urldecode($_GET["url"]);
-    $sitemaps = simplexml_load_file(sprintf("%s/sitemap.xml", $url));
+    $base_url = urldecode($_GET["url"]);
+    $urls = array();
+    $robots = file_get_contents(sprintf("%s/robots.txt", $base_url));
+    $sitemaps = explode("Sitemap:", $robots);
 
-    if ($sitemaps === false) {
-        try {
-            $file_robots = file_get_contents(sprintf("%s/robots.txt", $url));
-            $res_match = preg_match("/(Sitemap:)(.+)/", $file_robots, $str_matches);
+    for ($i = 1; $i < sizeof($sitemaps); $i++) {
+        $url = trim($sitemaps[$i]);
 
-            if (!$res_match) {
-                throw new Exception();
-            }
-
-            $url = trim(array_pop($str_matches));
-            $sitemaps = simplexml_load_file($url);
-        } catch (Exception $e) {
-            die(json_encode(array("error" => 1, "message" => "This site doesn't have a sitemap.xml! Try another site!")));
-        }
+        if (filter_var($url, FILTER_VALIDATE_URL))
+            $urls[] = $url;
+        else
+            $urls[] = $base_url . $url;
     }
 
-    // if it has a sitemap.xml file
+    global $page_uris;
     $page_uris = array();
 
-    // iterate through sitemap locations and urls, and fill the URI's array
-    foreach ($sitemaps as $sitemap) {
-        $urlset = simplexml_load_file($sitemap->loc);
-
-        foreach ($urlset as $url) {
-            $page_uris[] = $url->loc;
-        }
+    foreach ($urls as $url) {
+        page_counter($url, $page_uris);
     }
 
-    // remove duplicates
     $page_uris = array_unique($page_uris);
+    printf(sizeof($page_uris));
+
+    // $sitemaps = simplexml_load_file($url);
+
+    // // if it has a sitemap.xml file
+    // $page_uris = array();
+
+    // // iterate through sitemap locations and urls, and fill the URI's array
+    // foreach ($sitemaps as $sitemap) {
+    //     $urlset = simplexml_load_file($sitemap->loc);
+
+    //     foreach ($urlset as $url) {
+    //         $page_uris[] = $url->loc;
+    //     }
+    // }
+
+    // // remove duplicates
+    // $page_uris = array_unique($page_uris);
     // generate json response
-    print(json_encode(array("error" => 0, "message" => sprintf("This site have %d pages!", sizeof($page_uris)))));
+    // print(json_encode(array("error" => 0, "message" => sprintf("This site have %d pages!", sizeof($page_uris)))));
 } else {
-    print(json_encode(array("error" => 1, "message" => "Please, set the url parameter!")));
+    // print(json_encode(array("error" => 1, "message" => "Please, set the url parameter!")));
+}
+
+function page_counter($url) {
+    $xml = simplexml_load_file($url);
+
+    if (isset($xml->sitemap)) {
+        foreach ($xml as $sitemap) {
+            page_counter($sitemap->loc);
+        }
+    } else {
+        foreach ($xml as $urlset_url) {
+            $page_uris[] = $urlset_url->loc;
+        }
+    }
 }
 ?>
